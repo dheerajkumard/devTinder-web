7ed2d26ab2fd1daee2ccbc8ec0978235d8e54cfb
dheerajkumard <dheerajkumarddk@gmail.com>
2025-08-12 14:21:52 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./NavBar"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold">Hello World</h1>
+     {/* <Navbar/> */}
+     <BrowserRouter>
+     <Routes basename="/">
+     <Route path="/" element={<Body/>} >
+      <Route path="/login" element={<Login/>} />
+      <Route path="/profile" element={<Profile/>} />
+     </Route>
+     </Routes>
+     </BrowserRouter>
+      {/* <h1 class="text-3xl font-bold">Hello World</h1> */}
     </>
   )
 }
