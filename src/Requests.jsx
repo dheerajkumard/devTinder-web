33d5741d370d661c48c849932a77b4c45bb9129a
dheerajkumard <dheerajkumarddk@gmail.com>
2025-08-21 07:39:37 +0530

    renamed the buttons

--- src/Requests.jsx
+++ src/Requests.jsx
@@ -5,41 +5,42 @@ import { useDispatch, useSelector } from "react-redux";
 import { addRequests } from "./utils/requestSlice";
 
 const Requests = () => {
-   const dispatch = useDispatch();
-  const requests = useSelector((store)=> store.request);
-  const fetchRequests = async () => {
-    try {
-      const res = await axios.get(BASE_URL + "/user/requests/received", {
-        withCredentials: true}
-      );
-        dispatch(addRequests(res.data.data));
-        console.log(res.data.data);
+    const dispatch = useDispatch();
+    const requests = useSelector((store) => store.request);
+    const fetchRequests = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/user/requests/received", {
+                withCredentials: true
+            }
+            );
+            dispatch(addRequests(res.data.data));
+            console.log(res.data.data);
+        }
+        catch (error) {
+            console.error("Error fetching requests:", error);
+        }
     }
-    catch (error) {
-      console.error("Error fetching requests:", error);
+
+    useEffect(() => {
+        fetchRequests();
+    }, []);
+
+    if (!requests) return;
+    if (requests.length === 0) {
+        return <div className="flex justify-center my-10">No Requests found.</div>;
     }
-  }
-
-  useEffect(() => {
-    fetchRequests();
-  }, []);
-
-  if (!requests) return;
-  if (requests.length === 0) {
-    return <div className="flex justify-center my-10">No Requests found.</div>;
-  }
-  return (
-    <div className="text-center">
-     <h1 className="text-2xl">Requests</h1> 
-      {requests.map((request) => (
-      <div>
-        {request.fromUserId.firstName} {request.fromUserId.lastName}
-        <button className="btn btn-active btn-success mx-2 my-2">Success</button>
-<button className="btn btn-active btn-warning mx-2 my-2">Warning</button>
-      </div>
-    ))}
-    </div>
-  );
+    return (
+        <div className="text-center">
+            <h1 className="text-2xl">Requests</h1>
+            {requests.map((request) => (
+                <div>
+                    {request.fromUserId.firstName} {request.fromUserId.lastName}
+                    <button className="btn btn-active btn-success mx-2 my-2">Accept</button>
+                    <button className="btn btn-active btn-warning mx-2 my-2">Reject</button>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default Requests;
