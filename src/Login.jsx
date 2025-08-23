60638196d9dffac5bbbc0d6513011d5e91308acd
dheerajkumard <dheerajkumarddk@gmail.com>
2025-08-23 08:09:08 +0530

    feat: added signup api call

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,11 +6,27 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "./utils/constants";
 
 const Login = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('test@gmail.com');
   const [password, setPassword] = useState('Dheeru@123');
   const [error, setError] = useState('');
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const handleSignup = async () => {
+    try{
+      const res = await axios.post(BASE_URL + "/signup", {firstName, lastName, email, password}, {withCredentials: true});
+      console.log(res.data.data);
+      dispatch(addUser(res.data.data));
+      navigate("/profile");
+    }
+    catch (error) {
+      setError("Signup failed");
+      console.error("Signup failed:", error);
+    }
+  }
 
   const handleLogin = async () => {
     try {const res = await axios.post(BASE_URL + "/login", {
@@ -28,7 +44,14 @@ const Login = () => {
     <div className="my-10 flex justify-center">
       <div className="card bg-base-100 w-96 shadow-sm">
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Login</h2>
+          <h2 className="card-title">{isLoginForm ? "Login" : "Signup"}</h2>
+          {!isLoginForm && <><fieldset className="fieldset">            
+            <input type="text" value={firstName} className="input" placeholder="First Name" onChange={(e)=> setFirstName(e.target.value)}/>
+          </fieldset>
+          <fieldset className="fieldset">            
+            <input type="text" value= {lastName} className="input" placeholder="Last Name" onChange = {(e)=> setLastName(e.target.value)}/>
+          </fieldset>
+          </>}
           <fieldset className="fieldset">            
             <input type="text" value={email} className="input" placeholder="Email" onChange={(e)=> setEmail(e.target.value)}/>
           </fieldset>
@@ -37,8 +60,14 @@ const Login = () => {
           </fieldset>
           <p className="text-red-500">{error}</p>
           <div className="card-actions">
-            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+            {isLoginForm && <button className="btn btn-primary" onClick={handleLogin}>Login</button>}
+            {!isLoginForm && <button className="btn btn-primary" onClick={handleSignup}>Signup</button>}
           </div>
+          <div>
+            {isLoginForm ? 
+              <p>New user? <button className="text-blue-500" onClick={() => {setIsLoginForm(false)}}>Create an account</button></p> 
+              : <p>Already have an account? <button className="text-blue-500" onClick={() => {setIsLoginForm(true)}}>Login</button></p>}
+              </div>
         </div>
       </div>
     </div>
